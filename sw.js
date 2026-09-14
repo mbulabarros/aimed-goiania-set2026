8447f2045cab8ce0b93fa1772a3948143a0a1b91
mbulabarros <mbulabarros@gmail.com>
2026-09-14 15:03:35 +0000

    Adiciona os 16 vídeos verticais de abertura de módulo
    Preenche o campo avatar dos 16 módulos com os links dos vídeos
    publicados no YouTube (HeyGen, avatar de IA), mantendo index.html
    e AIMED_Goiania_19-20_09_2026.html idênticos. Cache do sw.js
    avançado de v15 para v31.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aimed-goiania-2026-v15';
+const CACHE = 'aimed-goiania-2026-v31';
 const LOCAL = [
   './', './index.html', './AIMED_Goiania_19-20_09_2026.html',
   './manifest.json', './og-image.png',
