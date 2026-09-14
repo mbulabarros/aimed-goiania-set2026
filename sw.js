cfb87211e8d64563e8593658bb5e9023cea61e7e
mbulabarros <132103735+mbulabarros@users.noreply.github.com>
2026-09-14 07:29:02 -0300

    Inclui instalação do Gemini Notebook na logística

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aimed-goiania-2026-v8';
+const CACHE = 'aimed-goiania-2026-v9';
 const LOCAL = [
   './', './index.html', './AIMED_Goiania_19-20_09_2026.html',
   './manifest.json', './og-image.png',
