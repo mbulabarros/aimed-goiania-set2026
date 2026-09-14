fcb7eb102b91cd6592883aa8f34c90feb0090661
mbulabarros <132103735+mbulabarros@users.noreply.github.com>
2026-09-14 00:02:13 -0300

    Melhora navegação do curso em iOS e Android

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aimed-goiania-2026-v2';
+const CACHE = 'aimed-goiania-2026-v3';
 const LOCAL = [
   './', './index.html', './AIMED_Goiania_19-20_09_2026.html',
   './manifest.json', './og-image.png',
