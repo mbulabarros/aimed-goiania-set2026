2c4c7d348cd196d75e150b0018f8a0ab99e24c71
mbulabarros <132103735+mbulabarros@users.noreply.github.com>
2026-09-14 07:52:49 -0300

    Amplia créditos do instrutor e inclui INOVAMED

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aimed-goiania-2026-v12';
+const CACHE = 'aimed-goiania-2026-v13';
 const LOCAL = [
   './', './index.html', './AIMED_Goiania_19-20_09_2026.html',
   './manifest.json', './og-image.png',
